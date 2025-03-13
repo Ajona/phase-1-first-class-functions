9c463ba9c7d06f4af3378613d87839d561af4daa
Warioba <ajonawarioba@gmail.com>
2025-03-13 13:41:28 +0300

    Done

---
+++ index.js
@@ -0,0 +1,25 @@
+// Function that receives a callback function and calls it
+function receivesAFunction(callback) {
+    callback();
+}
+
+// Function that returns a named function
+function returnsANamedFunction() {
+    return function namedFunction() {
+        console.log("I am a named function!");
+    };
+}
+
+// Function that returns an anonymous function
+function returnsAnAnonymousFunction() {
+    return function() {
+        console.log("I am an anonymous function!");
+    };
+}
+
+// Exporting the functions if needed for testing
+module.exports = {
+    receivesAFunction,
+    returnsANamedFunction,
+    returnsAnAnonymousFunction
+};
